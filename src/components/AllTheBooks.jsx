b147e879856eb2b1aa3390f6dc746d8f2d126e8b
DennisLoiak11 <dennisloiak11@gmail.com>
2023-03-29 17:23:59 +0200

    fix-card

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,23 +1,29 @@
-import libri from '../data/fantasy.json'
+import books from '../data/fantasy.json'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function AllTheBooks() {
     return (
-        <>
-        <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                {libri.map((dish, index) => (
-                    <>
-                    <Card.Img variant="top" src={dish.img} />
-                    <Card.Title>{dish.title}</Card.Title>
-                    <Badge pill bg="dark">{dish.price}</Badge>
-                    </>
+        <Container>
+            <Row md={4}>
+                {books.map((book, index) => (
+                    <Col key={index}>
+                        <Card>
+                            <Card.Body>
+                                <Card.Img variant="top" src={book.img} />
+                                <Card.Title>{book.title}</Card.Title>
+                                <Badge pill bg="dark">{book.price}</Badge>
+                            </Card.Body>
+                        </Card>
+                    </Col>
                 ))}
-            </Card.Body>
-        </Card>
-        </>
+            </Row>
+        </Container>
     );
 }
 
